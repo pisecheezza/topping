4c712fc1a75a5f6287d8da3362b125fc7518d5a1
pisecheezza <levelcmxcix@gmail.com>
2026-07-25 11:42:01 +0900

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 // 1) 구글 스프레드시트를 "링크가 있는 모든 사용자 - 보기 가능"으로 공유 설정
 // 2) 시트 URL에서 /d/ 와 /edit 사이의 긴 문자열을 SHEET_ID에 붙여넣기
 //    예: https://docs.google.com/spreadsheets/d/여기부분/edit
-const SHEET_ID = "여기에_스프레드시트_ID_입력";
+const SHEET_ID = "14DVjPLKuq4pkBjoAw_h1J0BanWgJif4jNSw-6qhU4q0";
 
 // 3) 각 탭(시트)의 이름이 아래와 다르면 맞춰서 수정
 const TABS = {
@@ -14,8 +14,8 @@ const TABS = {
 
 // 4) 사이트 제목 / 한줄소개 기본값 (Profile 시트에 name/tagline 키가 있으면 그 값이 우선 적용됨)
 const SITE_DEFAULTS = {
-  name: "이름",
-  tagline: "한 줄 소개"
+  name: "Sage",
+  tagline: "個人HPと言う空間を作り出すのだ。"
 };
 
 // 드라이브 파일 ID로 이미지 직링크 만들기
