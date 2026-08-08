ee99cb55b8241776af7927e75db6c38d4ff95408
pisecheezza <levelcmxcix@gmail.com>
2026-08-08 15:29:50 +0900

    Update player.js

--- player.js
+++ player.js
@@ -59,14 +59,11 @@
         playlist: [
 
             { url: "https://www.youtube.com/watch?v=zbKWAzNF6ao", title: "SHIVER" },
-            { url: "https://www.youtube.com/watch?v=ApJ-vd-9RU0", title: "Fragile Mind" },
             { url: "https://www.youtube.com/watch?v=Ol7PzD0iwMk", title: "The Slightly Chipped Full Moon" },
-            { url: "https://www.youtube.com/watch?v=AePhG6R6UdE", title: "Cruelly Eyes" },
             { url: "https://www.youtube.com/watch?v=ulp9XrKjKQE", title: "Suo Gân by Emilie Parry Williams" },
             { url: "https://www.youtube.com/watch?v=Ui9YLBHq490", title: "輝く空の静寂には" },
             { url: "https://www.youtube.com/watch?v=kWk95U_njJ0", title: "Medieval Style" },
             { url: "https://www.youtube.com/watch?v=iv5vSbC6188", title: "The Butler of Trancy's" },
-            { url: "https://www.youtube.com/watch?v=Nx5c_JZIM6M", title: "Summer, III. Presto by Vivaldi, RV315" },
             { url: "https://www.youtube.com/watch?v=RuNqttY67O4", title: "A Spider's Thread" },
          ]
     };
