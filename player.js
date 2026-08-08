522ade4782a042430eb5a69cae78d420914796a6
pisecheezza <levelcmxcix@gmail.com>
2026-08-08 15:03:39 +0900

    Update player.js

--- player.js
+++ player.js
@@ -24,7 +24,6 @@
 
         if (isReload) {
             sessionStorage.setItem('lp_playing', 'false');
-        
         }
 
         var savedVol = sessionStorage.getItem('lp_volume');
@@ -55,25 +54,22 @@
         }
     })();
 
-
-    /* ===========================================
-       [사용자 설정2] 노래 목록, 이미지, 볼륨 설정
-       =========================================== */
     const LP_CONFIG = {
-
-        // 1. 기본 볼륨 (0 ~ 100 사이)
         defaultVolume: 70,
-
-        // 2. 플레이리스트 (유튜브 링크와 제목을 입력하세요)
         playlist: [
 
             { url: "https://www.youtube.com/watch?v=zbKWAzNF6ao", title: "SHIVER" },
+            { url: "https://www.youtube.com/watch?v=ApJ-vd-9RU0", title: "Fragile Mind" },
             { url: "https://www.youtube.com/watch?v=Ol7PzD0iwMk", title: "The Slightly Chipped Full Moon" },
+            { url: "https://www.youtube.com/watch?v=AePhG6R6UdE", title: "Cruelly Eyes" },
             { url: "https://www.youtube.com/watch?v=ulp9XrKjKQE", title: "Suo Gân by Emilie Parry Williams" },
             { url: "https://www.youtube.com/watch?v=Ui9YLBHq490", title: "輝く空の静寂には" },
             { url: "https://www.youtube.com/watch?v=kWk95U_njJ0", title: "Medieval Style" },
             { url: "https://www.youtube.com/watch?v=iv5vSbC6188", title: "The Butler of Trancy's" },
+            { url: "https://www.youtube.com/watch?v=Nx5c_JZIM6M", title: "Summer, III. Presto by Vivaldi, RV315" },
             { url: "https://www.youtube.com/watch?v=RuNqttY67O4", title: "A Spider's Thread" },
+            { url: "https://www.youtube.com/watch?v=7eNmrTcpW9g", title: "Cena d'amore" },
+            { url: "https://www.youtube.com/watch?v=OM9KdvA7D7E", title: "Love Remembered from Dracula" },
          ]
     };
 
